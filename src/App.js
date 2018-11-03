069de80a0477a1bf06a34b46f77aab4ecb83d5eb
Marco Barão <marco.barao@outlook.com>
2018-11-03 16:39:56 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-import Header from "./components/Header/index";
+import Header from "./components/Header";
+import Routes from "./routes";
 
 import "./styles.css";
 
 const App = () => (
   <div className="App">
     <Header />
+    <Routes />
   </div>
 );
 
